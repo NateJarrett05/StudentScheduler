5ea58b5a2e1a28dec0f545bdc870888a65f70d0c
Nate Jarrett <NateJarrett05@gmail.com>
2024-07-16 15:30:31 -0700

    Converted school and counter into their own files. Put functions for accessing periods within the school file. Functions to access the counters will be implemented in the counter file if needed.

--- src/counter.js
+++ src/counter.js
@@ -3,7 +3,7 @@ const { School } = require("./school");
 class Counter extends School{
     constructor(){
       super();
-      this.allPeriodCounters = [
+      this.allPeriodCounter = [
           [0, 0, 0, 0],
           [0, 0, 0, 0],
           [0, 0, 0, 0],
@@ -14,40 +14,40 @@ class Counter extends School{
 
     //Watch out for the variables in school not updated properly because this is an instance of the class?
     checkFull(){
-      for(let i = 0; i < this.allPeriodCounters.length; i++){
-        for(let k = 0; k < this.allPeriodCounters[i].length; k++){
-          if(this.allPeriodCounters[i][k] >= class_size){
-            this.school.allPeriods[i].splice(k, 1);
-            this.allPeriodCounters[i].splice(k, 1);
+      for(let i = 0; i < this.allPeriodCounter.length; i++){
+        for(let k = 0; k < this.allPeriodCounter[i].length; k++){
+          if(this.allPeriodCounter[i][k] >= class_size){
+            this.remove_period(i, k);
+            this.allPeriodCounter[i].splice(k, 1);
           }
         }
       }
     }
 
-    increaseCounter(schedule){
+    increase_counter(schedule){
       for(let y = 0; y < schedule.length; y++){
-        this.allPeriodCounters[y][this.school.allPeriods[y].indexOf(schedule[y])]++
+        this.allPeriodCounter[y][this.get_class_index(y, schedule[y])]++;
       }
     }
 
     increaseLowRSP(){
-      this.allPeriodCounters[0][this.school.allPeriods[0].indexof(this.school.lowRSP_Schedule[0])]++;
-      this.allPeriodCounters[1][this.school.allPeriods[1].indexof(this.school.lowRSP_Schedule[1])]++;
-      this.allPeriodCounters[2][this.school.allPeriods[2].indexof(this.school.lowRSP_Schedule[2])]++;
-      this.allPeriodCounters[3][this.school.allPeriods[3].indexof(this.school.lowRSP_Schedule[3])]++;
-      this.allPeriodCounters[4][this.school.allPeriods[4].indexof(this.school.lowRSP_Schedule[4])]++;
+      this.allPeriodCounter[0][this.get_class_index(0, this.return_lowRSP_period(0))]++;
+      this.allPeriodCounter[1][this.get_class_index(0, this.return_lowRSP_period(1))]++;
+      this.allPeriodCounter[2][this.get_class_index(0, this.return_lowRSP_period(2))]++;
+      this.allPeriodCounter[3][this.get_class_index(0, this.return_lowRSP_period(3))]++;
+      this.allPeriodCounter[4][this.get_class_index(0, this.return_lowRSP_period(4))]++;
     }
 
     increaseHighRSP(){
-      this.allPeriodCounters[0][this.school.allPeriods[0].indexof(this.school.highRSP_Schedule[0])]++;
-      this.allPeriodCounters[1][this.school.allPeriods[1].indexof(this.school.highRSP_Schedule[1])]++;
-      this.allPeriodCounters[2][this.school.allPeriods[2].indexof(this.school.highRSP_Schedule[2])]++;
-      this.allPeriodCounters[3][this.school.allPeriods[3].indexof(this.school.highRSP_Schedule[3])]++;
-      this.allPeriodCounters[4][this.school.allPeriods[4].indexof(this.school.highRSP_Schedule[4])]++;
+      this.allPeriodCounter[0][this.get_class_index(0, this.return_highRSP_period(0))]++;
+      this.allPeriodCounter[1][this.get_class_index(0, this.return_highRSP_period(1))]++;
+      this.allPeriodCounter[2][this.get_class_index(0, this.return_highRSP_period(2))]++;
+      this.allPeriodCounter[3][this.get_class_index(0, this.return_highRSP_period(3))]++;
+      this.allPeriodCounter[4][this.get_class_index(0, this.return_highRSP_period(4))]++;
     }
 
-    reset(){
-      this.allPeriodCounters = [
+    reset_counter(){
+      this.allPeriodCounter = [
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
--- src/school.js
+++ src/school.js
@@ -45,26 +45,46 @@ class School {
         for(let k = 0; k < this.classList.length; k++){
             var checkingClass = this.classList[k]
             for(let m = 0; m < periodList.length; m++){
-            for(let j = 0; j < periodList[m].length; j++){
-                //if the period contains the class im checking, add the period to that class's list
-                if(periodList[m][j] == checkingClass){
-                availablePeriods[k].push(m)
+                for(let j = 0; j < periodList[m].length; j++){
+                    //if the period contains the class im checking, add the period to that class's list
+                    if(periodList[m][j] == checkingClass){
+                    availablePeriods[k].push(m)
+                    }
                 }
-            }
             }
         }
         return availablePeriods;
     }
 
-    getClass(period){
+    get_random_class(period){
         return Math.floor(Math.random() * this.allPeriods[period].length);
+    }
+
+    get_class_index(period, class_name){
+        return this.allPeriods[period].indexOf(class_name);
+    }
+
+    return_class(period, classIndex){
+        return this.allPeriods[period][classIndex];
+    }
+
+    return_lowRSP_period(period){
+        return this.lowRSP_Schedule[period];
+    }
+
+    return_highRSP_period(period){
+        return this.highRSP_Schedule[period];
     }
 
     checkPeriod(schedule, period){
         return (schedule.indexOf(this.allPeriods[period][period]) == -1)
     }
 
-    reset(){
+    remove_period(period, class_index){
+        this.allPeriods[period].splice(class_index, 1);
+    }
+
+    reset_periods(){
         this.allPeriods = (gradeSelection == 7) ? [
             ["english", "science", "PE", "math"],
             ["english", "science", "PE", "history"],
